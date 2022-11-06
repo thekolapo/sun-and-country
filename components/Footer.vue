1ab2cf6093398c2ef5d473b5d8ba6eb2eaebc2e9
Kolapo Oni <onikolapo@gmail.com>
2022-11-06 22:33:01 +0100

    create nav & footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,24 @@
 <template>
-  <footer class="footer"></footer>
+  <footer class="footer">
+    <div class="footer__copyright">Vistanium © 2022</div>
+    <div class="footer__links">
+      <a class="c-link" href="" target="_blank">Twitter</a>
+      <a class="c-link" href="" target="_blank">Instagram</a>
+    </div>
+  </footer>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  position: absolute;
+
+  &__links a:first-child {
+    margin-right: 3rem;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -8,4 +8,12 @@
 
 <style lang="scss">
 @import '~/assets/scss/main.scss';
+
+.app {
+  position: relative;
+  max-width: 124rem;
+  margin: 0 auto;
+  padding: 4.6rem 0 10rem;
+  min-height: 90vh;
+}
 </style>
