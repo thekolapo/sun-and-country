ababc685fe4f75c421c071401bcaf1cf68ba45e4
Kolapo Oni <onikolapo@gmail.com>
2022-11-06 23:27:58 +0100

    make nav & footer sticky

--- components/Footer.vue
+++ components/Footer.vue
@@ -10,12 +10,11 @@
 
 <style lang="scss" scoped>
 .footer {
-  position: absolute;
-  bottom: 0;
+  position: sticky;
+  bottom: 12rem;
   width: 100%;
   display: flex;
   justify-content: space-between;
-  position: absolute;
 
   &__links a:first-child {
     margin-right: 3rem;
