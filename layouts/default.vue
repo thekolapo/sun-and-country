9d22400b75b58aa5b5a3d990779336898ffef1aa
Kolapo Oni <onikolapo@gmail.com>
2022-11-11 21:10:04 +0100

    implement homepage

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,6 +6,18 @@
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {}
+  },
+  created() {
+    if (process.browser)
+      sessionStorage.setItem('triggeredHomepageAnimation', false)
+  },
+}
+</script>
+
 <style lang="scss">
 @import '~/assets/scss/main.scss';
 
@@ -13,6 +25,7 @@
   position: relative;
   width: 92%;
   max-width: 124rem;
+  // min-height: 82vh;
   min-height: 90vh;
   margin: 0 auto;
   padding: 0 0rem 8rem;
